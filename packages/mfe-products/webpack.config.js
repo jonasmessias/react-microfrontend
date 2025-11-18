98ae9b0b3d388dcaf0e4475c3289c305aef35274
Jonas <jonasmessias30@gmail.com>
2025-11-18 01:48:27 -0300

    feat(mfe-products): initialize products microfrontend with module federation

---
+++ packages/mfe-products/webpack.config.js
@@ -0,0 +1,79 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+
+  devServer: {
+    port: 3001,
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+
+  output: {
+    publicPath: 'auto',
+    clean: true,
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                config: path.resolve(__dirname, 'postcss.config.js'),
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'mfeProducts',
+      filename: 'remoteEntry.js',
+
+      exposes: {
+        './Products': './src/Products',
+      },
+
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: '^18.2.0',
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: '^18.2.0',
+        },
+        zustand: {
+          singleton: true,
+        },
+      },
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
