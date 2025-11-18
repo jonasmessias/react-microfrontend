67004de0a74f00e5767f63c45c2637fb784cb448
Jonas <jonasmessias30@gmail.com>
2025-11-18 01:49:42 -0300

    feat(mfe-cart): integrate Tailwind CSS with design system

---
+++ packages/mfe-cart/tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [require('../design-system/tailwind.config')],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  plugins: [],
+};
